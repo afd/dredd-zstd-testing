7d3c4196a705fee5279ba6600f72c0ae6669cc29
Alastair F. Donaldson <alastair.donaldson@imperial.ac.uk>
2026-06-24 05:11:23 +0100

    Pin specific clang version.

--- build-datagen.sh
+++ build-datagen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clang -O2 -I lib/common -I programs -I tests \
+clang-17 -O2 -I lib/common -I programs -I tests \
     programs/datagen.c programs/lorem.c tests/loremOut.c tests/datagencli.c \
     -o datagen
--- build-roundtrip.sh
+++ build-roundtrip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clang dredd-zstd-testing/roundtrip.c -I lib build-zstd/lib/libzstd.a -o roundtrip
+clang-17 dredd-zstd-testing/roundtrip.c -I lib build-zstd/lib/libzstd.a -o roundtrip
